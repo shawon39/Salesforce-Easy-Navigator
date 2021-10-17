c1aeb896b4bb72f2d8a3c25d3e2d9f550b255856
Shakhawat <shakhawat.hossain@bjitgroup.com>
2021-10-17 10:27:20 +0600

    Added setup devConsole Objmanager Language & Time Zone

---
+++ openUrl.js
@@ -0,0 +1,14 @@
+// List of all page
+
+openSetup = () => {
+	return "/lightning/setup/SetupOneHome/home";
+};
+openDevConsole = () => {
+	return "/_ui/common/apex/debug/ApexCSIPage";
+};
+openObjManager = () => {
+	return "/lightning/setup/ObjectManager/home";
+};
+openLanguage = () => {
+	return "/lightning/settings/personal/LanguageAndTimeZone/home";
+};
--- popup.js
+++ popup.js
@@ -1,24 +1,55 @@
-/* Popup.js */
+// import { openSetup, openDevConsole } from "./openUrl";
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", () => {
+	// Getting Setup
+	let setup = document.getElementById("setup");
+	if (setup) {
+		setup.addEventListener("click", () => {
+			getCurrentTab("setup");
+		});
+	}
+	// Getting devConsole
+	let devConsole = document.getElementById("devConsole");
+	if (devConsole) {
+		devConsole.addEventListener("click", () => {
+			getCurrentTab("devConsole");
+		});
+	}
+    // Getting ObjManager
+    let objManager = document.getElementById("objManager");
+	if (objManager) {
+		objManager.addEventListener("click", () => {
+			getCurrentTab("objManager");
+		});
+	}
+    // Getting Language & Time Zone
+    let language = document.getElementById("language");
+	if (objManager) {
+		language.addEventListener("click", () => {
+			getCurrentTab("language");
+		});
+	}
 
-    let setup = document.getElementById("setup");
+});
 
-    if(setup) {
-        setup.addEventListener("click", () => {
-            getCurrentTab();
-        });
-    }
-
-}, false);
-
-async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    let baseUrl = 'https://platformdeveloper3-dev-ed.lightning.force.com';
-    openSetup(baseUrl);
+async function getCurrentTab(page) {
+	let queryOptions = { active: true, currentWindow: true };
+	let [tab] = await chrome.tabs.query(queryOptions);
+	let fullUrl = tab.url;
+	let baseUrl = fullUrl.substring(0, fullUrl.indexOf("/", 10));
+	openUrl(baseUrl, page);
 }
 
-function openSetup(baseUrl) {
-    chrome.tabs.create({active: true, url: baseUrl + '/lightning/setup/SetupOneHome/home'});
-}
+openUrl = (baseUrl, page) => {
+	if (page === "setup") {
+		chrome.tabs.create({ active: true, url: baseUrl + openSetup() });
+	} else if (page === "devConsole") {
+		chrome.tabs.create({ active: true, url: baseUrl + openDevConsole() });
+	} else if (page === "objManager") {
+		chrome.tabs.create({ active: true, url: baseUrl + openObjManager() });
+	} else if (page === "language") {
+		chrome.tabs.create({ active: true, url: baseUrl + openLanguage() });
+	}
+
+	//chrome.tabs.update({url: baseUrl + '/lightning/setup/SetupOneHome/home'});
+};
